92935d9cdbc53305a7392d3b425c946043c966b6
ollyjw <owilcocks@gmail.com>
2024-01-23 15:50:25 +0000

    created intial cv data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const cvData = {
+    generalInfo: {
+        name: "",
+        email: "",
+        phone: "",
+    }, 
+    education: [{
+        school: "",
+        subject: "",
+        dates: "",
+        id: 0,
+    }], 
+    workExp: [{
+        company: "",
+        position: "",
+        description: "",
+        dates: "",
+        id: 0,
+    }],
+    skills: [{
+        skill: "",
+        id: 0
+    }],
+    references: [{
+        name: "",
+        jobTitle: "",
+        email: "",
+        id: 0,
+    }]
+};
+
+export default cvData;
